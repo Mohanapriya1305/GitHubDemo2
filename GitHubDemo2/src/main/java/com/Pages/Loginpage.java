a7d7781ea026012da9c3ab4f0c95365f530fd945
user <user@DESKTOP-BF8K1BJ>
2023-12-29 13:08:44 +0530

    first commit

---
+++ GitHubDemo2/src/main/java/com/Pages/Loginpage.java
@@ -0,0 +1,9 @@
+package com.Pages;
+
+public class Loginpage {
+
+	public void loginpage() {
+		System.out.println("This is login page");
+		System.out.println("Now, we add new text file");
+	}
+}
---
+++ GitHubDemo2/src/test/java/com/Testcases/LoginTest.java
@@ -0,0 +1,8 @@
+package com.Testcases;
+
+public class LoginTest {
+
+	public void logintest() {
+		System.out.println("This is login test");
+	}
+}
